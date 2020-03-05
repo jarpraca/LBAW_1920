11c0ff0baf7d3d2e7f967489104e61bc674a8d15
silviavrocha <silviajmr99@gmail.com>
2020-03-05 14:51:42 +0000

    Homepage almost done, started auction details
    Co-authored-by: João Praça <jarpraca@gmail.com>

--- homepage.php
+++ homepage.php
@@ -11,19 +11,76 @@ if (!isset($_SESSION['csrf'])) {
 }
 
 $logedin = false;
-$displaySearch = false;
+$displaySearch = true;
 
 if (isset($_SESSION['user']))
     $logedin = true;
 
 include('templates/common/header.php');
 ?>
-<div id="homepage">
-    <h2 id=>Welcome</h2>
-    <div class="homepageForm">
-        <?php
-        include('templates/common/search.php');
-        ?>
+<div class="text-left" id="homepage">
+    <h2 class="mt-3">Welcome, </h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et pulvinar dui, tincidunt faucibus tortor. Curabitur feugiat quam arcu. Nulla malesuada quis neque sed ultrices. Vivamus non purus et ipsum ultricies ornare sit amet eget erat. Nam quam ex, sodales sit amet dapibus pellentesque, suscipit quis eros. Nulla vestibulum dapibus urna, non fringilla magna faucibus vel. Nam pharetra sapien nisl, ut rutrum lacus pulvinar lacinia. Phasellus justo lorem, venenatis id libero a, varius vehicula arcu. Mauris eget urna urna. Etiam quam nulla, ultrices non est ac, laoreet blandit nisi. Morbi quis leo nec tellus consectetur consequat ut et massa. Nam laoreet ipsum quis finibus fermentum. Praesent sit amet nisl luctus, sollicitudin nulla sed, mattis elit.</p>
+    <div class="d-flex flex-row text-left">
+        <div class="auction">
+            <img width="100%" src="./images/profilePicture.jpg">
+            <p>Macaco da Nazaré</p>
+            <p>500€</p>
+            <p>2anos</p>
+        </div>
+        <div class="auction mx-5">
+            <img width="100%" src="./images/profilePicture.jpg">
+            <p>Macaco da Nazaré 2</p>
+            <p>500€</p>
+            <p>2anos</p>
+        </div>
+        <div class="auction">
+            <img width="100%" src="./images/profilePicture.jpg">
+            <p>Macaco da Nazaré 3</p>
+            <p>500€</p>
+            <p>2anos</p>
+        </div>
+    </div>
+
+    <div class="d-flex flex-row mt-5">
+        <div class="border border-dark container">
+            <img width="100%" src="./images/profilePicture.jpg">
+            <p class="overlay">Category 1</p>
+        </div>
+        <div class="border border-dark container">
+            <img width="100%" src="./images/profilePicture.jpg">
+            <p class="overlay">Category 2</p>
+
+        </div>
+        <div class="border border-dark container">
+            <img width="100%" src="./images/profilePicture.jpg">
+            <p class="overlay">Category 3</p>
+
+        </div>
+        <div class="border border-dark container">
+            <img width="100%" src="./images/profilePicture.jpg">
+            <p class="overlay">Category 4</p>
+        </div>
+    </div>
+    <div class="d-flex flex-row mb-5">
+        <div class="border border-dark container">
+            <img width="100%" src="./images/profilePicture.jpg">
+            <p class="overlay">Category 5</p>
+        </div>
+        <div class="border border-dark container">
+            <img width="100%" src="./images/profilePicture.jpg">
+            <p class="overlay">Category 6</p>
+
+        </div>
+        <div class="border border-dark container">
+            <img width="100%" src="./images/profilePicture.jpg">
+            <p class="overlay">Category 7</p>
+
+        </div>
+        <div class="border border-dark container">
+            <img width="100%" src="./images/profilePicture.jpg">
+            <p class="overlay">Category 8</p>
+        </div>
     </div>
 </div>
 <?php
--- templates/common/footer.php
+++ templates/common/footer.php
@@ -1,12 +1,12 @@
 </div>
-<footer class="footer">
+<footer class="h-100 footer text-center d-flex justify-content-between align-middle w-100">
     <!-- <h3>BidMonkeys</h3> -->
-    <h5>Copyright © 2020</h5>
-    <li class="alignCenter">
-        <a href="#">
+    <h5 class="mx-2 mt-3 mb-3 p-0 align-middle">Copyright © 2020</h5>
+    <li class="d-flex flex-row align-middle">
+        <a class="text-white mx-2 mt-3 mb-3 p-0 align-middle" href="#">
             <h5>Help</h5>
         </a>
-        <a href="#">
+        <a class="text-white mx-2 mt-3 mb-3 p-0 align-middle" href="#">
             <h5>About</h5>
         </a>
     </li>
