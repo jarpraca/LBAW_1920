62cbaad1070b73e0020b4d6a62cae97bfef16641
João Praça <up201704748@fe.up.pt>
2020-04-20 18:00:36 +0100

    Fixed header redirects

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -50,6 +50,6 @@ class LoginController extends Controller
 
     public function home()
     {
-        return redirect('login');
+        return redirect('homepage');
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
 
   <link rel="icon" href="{{asset('assets/logo.png')}}" type="image/x-icon" />
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
- 
+
   <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
@@ -72,16 +72,15 @@
             <img src="{{asset('assets/bell.png')}}" height="30" alt="Notifications">
           </div>
         </li>
-        @endauth
 
         <li class="nav-item">
           <a class="nav-link btn btn-add-auction mx-sm-2 px-2 <?php //if ($createAuctionPage) { 
                                                               ?> btn-darkGreen <?php // } 
                                                                                 ?>" <?php // if (!$createAuctionPage) { 
                                                                                     ?> id="btn-add-auction" <?php // } 
-                                                                                                                                              ?> href="/auctions">+ Add Auction</a>
+                                                                                                            ?> href="/auctions">+ Add Auction</a>
         </li>
-        @auth
+
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle text-center my-2 my-sm-0" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             {{Auth::user()->name}}
@@ -96,6 +95,14 @@
         </li>
         @endauth
         @guest
+        <li class="nav-item">
+          <a class="nav-link btn btn-add-auction mx-sm-2 px-2 <?php //if ($createAuctionPage) { 
+                                                              ?> btn-darkGreen <?php // } 
+                                                                                ?>" <?php // if (!$createAuctionPage) { 
+                                                                                    ?> id="btn-add-auction" <?php // } 
+                                                                                                            ?> href="/login">+ Add Auction</a>
+        </li>
+
         <li class="nav-item">
           <a class="nav-link btn my-2 my-sm-0 px-2<?php //if ($signUpPage) { 
                                                   ?> btn-darkGreen <?php //} 
